94dc0e25c8542ded8842e2552bbadb2892a58b5f
Ifeanyi Nneji <ifeanyinneji777@gmail.com>
2022-06-14 14:07:23 +0100

    Update app.py

--- Frontend/app.py
+++ Frontend/app.py
@@ -11,14 +11,12 @@ st.write("""
 
 Lung cancer is a type of cancer that begins in the lungs and most often occurs in people who smoke. Two major types of lung cancer are non-small cell lung cancer and small cell lung cancer. Causes of lung cancer include smoking, second-hand smoke, exposure to certain toxins and family history. Symptoms include a cough (often with blood), chest pain, wheezing and weight loss. These symptoms often don't appear until the cancer is advanced. Treatments vary but may include surgery, chemotherapy, radiation therapy, targeted drug therapy and immunotherapy.
 
-In this project I'll build a machine learning model that can help in detecting/predicting lung cancer based on the following features: age, gender, blood pressure, smoke, coughing, allergies, fatigue etc.
+In this project I built a machine learning model that can help in detecting/predicting lung cancer based on the following features: age, gender, blood pressure, smoke, coughing, allergies, fatigue etc. The model was then deployed as an API using the FastAPI framework and then accessed through this interface with Streamlit.
 
-Dataset Link:- The dataset used was gotten from this [Google Drive Link]((https://drive.google.com/file/d/1diV8akPbsadSAn0rFzcLB7b3L9lL8Olw/view))
+The notebook, processed dataset and proper documentation(dockerfiles, fastapi script, streamlit script) are available on my [GitHub](https://github.com/Nneji123/Lung-Cancer-Prediction)        
+
 
 **Made by Ifeanyi Nneji**
-
-The notebook, processed dataset and files(dockerfile, fastapi script, streamlit script) are available on my [GitHub](https://github.com/Nneji123/Lung-Cancer-Prediction)        
-
 """)
 
 st.sidebar.header('User Input Features')
@@ -26,9 +24,9 @@ st.sidebar.header('User Input Features')
 
 
 
-gender = st.sidebar.number_input("GENDER: Enter 1 for Male and O for Female", min_value=0, max_value=1)
-age = st.sidebar.number_input("Enter your Age",min_value=1, max_value=100)
-smoking = st.sidebar.number_input("SMOKING: Enter 1 if you smoke or O if you don't smoke", min_value=0, max_value=1)
+gender = st.sidebar.number_input("GENDER: Enter 1 for Male and 0 for Female", min_value=0, max_value=1)
+age = st.sidebar.number_input("AGE: Enter your Age",min_value=1, max_value=100)
+smoking = st.sidebar.number_input("SMOKING: Enter 1 if you smoke or 0 if you don't smoke", min_value=0, max_value=1)
 yellow_finger = st.sidebar.number_input("YELLOW FINGERS: Enter 1 if you have yellow fingers or 0 if you don't", min_value=0, max_value=1)
 anxiety = st.sidebar.number_input("ANXIETY: Enter 1 if you have anxiety and 0 if you don't", min_value=0, max_value=1)
 peer = st.sidebar.number_input("PEER PRESSURE: Enter 1 if you feel you suffer from peer pressure or 0 if you don't", min_value=0, max_value=1)
